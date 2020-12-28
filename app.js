baa04b03175c32a52cd8cccf0407011b91bc05e9
avshukan <avshukan@gmail.com>
2020-12-28 16:21:23 +1100

    Add routes/index and bin/www. Listening port 3010

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-const http = require("http");
+// const http = require("http");
  
-let message = "Hello World!";
-http.createServer(function(request,response){
+// let message = "Hello World!";
+// http.createServer(function(request,response){
      
-    console.log(message);
-    response.end(message);
+//     console.log(message);
+//     response.end(message);
      
-}).listen(3000, "127.0.0.1",()=>{
-    console.log("Сервер начал прослушивание запросов");
+// }).listen(3000, "127.0.0.1",()=>{
+//     console.log("Сервер начал прослушивание запросов");
+// });
+const createError = require('http-errors');
+const express = require('express');
+const indexRouter = require('./routes/index');
+
+
+const app = express();
+
+// app.use(logger(':remote-addr - :remote-user  [:date[iso]]  :method  :status  :res[content-length]  HTTP/:http-version  :url  :referrer'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(corsHeadersService);
+// app.use(mongoConnectorService);
+
+
+app.use('/', indexRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+module.exports = app;
