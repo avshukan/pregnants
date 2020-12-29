68fc3b54dfd16d0d5e7bd9367385de6d5c29b776
avshukan <avshukan@gmail.com>
2020-12-29 12:04:45 +1100

    Add env

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-// const http = require("http");
- 
-// let message = "Hello World!";
-// http.createServer(function(request,response){
-     
-//     console.log(message);
-//     response.end(message);
-     
-// }).listen(3000, "127.0.0.1",()=>{
-//     console.log("Сервер начал прослушивание запросов");
-// });
 const createError = require('http-errors');
 const express = require('express');
 const indexRouter = require('./routes/index');
+require('dotenv').config();
 
 
 const app = express();
--- routes/pregnants-info.js
+++ routes/pregnants-info.js
@@ -2,18 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async function(req, res, next) {
-  res.send({ message: 'Info' });
+  // res.send({ message: 'Info' });
+  const { ORACLE_DB_PATH, ORACLE_DB_HOST, ORACLE_DB_PORT, ORACLE_DB_NAME, ORACLE_DB_USER, ORACLE_DB_PASS } = process.env;
+  console.log(ORACLE_DB_PATH);
   let oracledb = require('oracledb')
   let connection;
   try {
-    oracledb.initOracleClient({libDir: 'C:\\Oracle\\instantclient_19_9'});
+    oracledb.initOracleClient({libDir: ORACLE_DB_PATH});
+    // oracledb.initOracleClient({libDir: 'C:\\Oracle\\instantclient_19_9'});
     console.error('Init!');
     oracledb
-      .getConnection({
-        user: 'miac',
-        password: 'defmiac',
-        connectString: 'localhost:1521/mis',
-      })
+    // .getConnection({
+    //   user: 'miac',
+    //   password: 'defmiac',
+    //   connectString: 'localhost:1521/mis',
+    // })
+    .getConnection({
+      connectString: `${ORACLE_DB_HOST}:${ORACLE_DB_PORT}/${ORACLE_DB_NAME}`,
+      user: ORACLE_DB_USER,
+      password: ORACLE_DB_PASS,
+    })
       .then((conn) => {
         connection = conn;
         console.error('Connection!', connection);
@@ -21,7 +29,8 @@ router.get('/', async function(req, res, next) {
         .execute(`SELECT COUNT(*) FROM DEV.D_AGENTS`)
         .then((result) => {
           console.log('execute!');
-          console.log(result)
+          // console.log(result);
+          res.send({ message: result });
         })
         .catch((err) => {
           console.error('Can\'t execute!');
