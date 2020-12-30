8ff1584f989cfc0a64c511112e65ef8e5fecce5d
Alexander Shukan <avshukan@gmail.com>
2020-12-30 15:41:40 +1100

    Add query and enp param

--- routes/pregnants-info.js
+++ routes/pregnants-info.js
@@ -2,21 +2,52 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async function(req, res, next) {
-  // res.send({ message: 'Info' });
-  const { ORACLE_DB_PATH, ORACLE_DB_HOST, ORACLE_DB_PORT, ORACLE_DB_NAME, ORACLE_DB_USER, ORACLE_DB_PASS } = process.env;
-  console.log(ORACLE_DB_PATH);
-  let oracledb = require('oracledb')
+  const { enp } = req.query;
+  console.log('enp', enp);
+  const { ORACLE_DB_HOST, ORACLE_DB_PORT, ORACLE_DB_NAME, ORACLE_DB_USER, ORACLE_DB_PASS } = process.env;
+  const query = `
+  WITH pregnants AS (
+    SELECT
+      ROW_NUMBER() OVER (PARTITION BY agent.id ORDER BY card.DATE_IN DESC NULLS FIRST) AS r,
+      agent.SURNAME,			-- Фамилия беременной
+      agent.FIRSTNAME,		-- Имя беременной
+      agent.LASTNAME,			-- Отчество беременной
+      agent.BIRTHDATE,		-- ДР беременной
+      agent.ENP,				-- ЕНП
+      agent.SNILS,			-- СНИЛС
+      card.DATE_IN,			-- Дата открытия карты беременной
+      card.DATE_OUT,			-- Дата закрытия карты беременной
+      pregnancy.BEGIN_DATE,	-- Дата начала срока
+      pregnancy.END_DATE,		-- Дата окончания срока
+      reason.NAME AS REASON  	-- Причина закрытия индивидуальной карты
+    FROM D_PREGNANT_CARDS card
+    INNER JOIN D_AGENTS agent
+      ON agent.ID = card.AGENT
+    INNER JOIN D_AGENT_PREGNANCY pregnancy
+      ON pregnancy.id = card.PREGNANCY
+    LEFT JOIN D_PREGC_OUT_REASONS reason
+      ON reason.ID = card.OUT_REASON
+  )
+  SELECT
+    SURNAME,
+    FIRSTNAME,
+    LASTNAME,
+    BIRTHDATE,
+    ENP,
+    SNILS,
+    DATE_IN,
+    DATE_OUT,
+    BEGIN_DATE,
+    END_DATE,
+    REASON
+  FROM PREGNANTS
+  WHERE 1 = 1
+    AND r = 1
+    AND ENP = :enp`;
+  let oracledb = require('oracledb');
   let connection;
   try {
-    oracledb.initOracleClient({libDir: ORACLE_DB_PATH});
-    // oracledb.initOracleClient({libDir: 'C:\\Oracle\\instantclient_19_9'});
-    console.error('Init!');
     oracledb
-    // .getConnection({
-    //   user: 'miac',
-    //   password: 'defmiac',
-    //   connectString: 'localhost:1521/mis',
-    // })
     .getConnection({
       connectString: `${ORACLE_DB_HOST}:${ORACLE_DB_PORT}/${ORACLE_DB_NAME}`,
       user: ORACLE_DB_USER,
@@ -24,13 +55,14 @@ router.get('/', async function(req, res, next) {
     })
       .then((conn) => {
         connection = conn;
-        console.error('Connection!', connection);
         connection
-        .execute(`SELECT COUNT(*) FROM DEV.D_AGENTS`)
+        .execute(query, [enp])
         .then((result) => {
           console.log('execute!');
           // console.log(result);
-          res.send({ message: result });
+          const answer = result.rows[0];
+          console.log(answer);
+          res.send(answer);
         })
         .catch((err) => {
           console.error('Can\'t execute!');
@@ -44,7 +76,7 @@ router.get('/', async function(req, res, next) {
     console.error('Whoops!');
     console.error(err);
     process.exit(1);
-  }  
+  }
 
 
   // const { createdOn = getNowDate() } = req.query;
