a436946ed01fc3358b732aae488cc7faae7c6328
Alexander Shukan <avshukan@gmail.com>
2021-01-11 16:18:32 +1100

    Remove query to separate file

--- routes/pregnants-info.js
+++ routes/pregnants-info.js
@@ -1,51 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
 
 router.get('/', async function(req, res, next) {
   const { enp } = req.query;
-  console.log('enp', enp);
   const { ORACLE_DB_HOST, ORACLE_DB_PORT, ORACLE_DB_NAME, ORACLE_DB_USER, ORACLE_DB_PASS } = process.env;
-  const query = `
-  WITH pregnants AS (
-    SELECT
-      ROW_NUMBER() OVER (PARTITION BY agent.id ORDER BY card.DATE_IN DESC NULLS FIRST) AS r,
-      agent.SURNAME,			-- Фамилия беременной
-      agent.FIRSTNAME,		-- Имя беременной
-      agent.LASTNAME,			-- Отчество беременной
-      agent.BIRTHDATE,		-- ДР беременной
-      agent.ENP,				-- ЕНП
-      agent.SNILS,			-- СНИЛС
-      card.DATE_IN,			-- Дата открытия карты беременной
-      card.DATE_OUT,			-- Дата закрытия карты беременной
-      pregnancy.BEGIN_DATE,	-- Дата начала срока
-      pregnancy.END_DATE,		-- Дата окончания срока
-      reason.NAME AS REASON  	-- Причина закрытия индивидуальной карты
-    FROM D_PREGNANT_CARDS card
-    INNER JOIN D_AGENTS agent
-      ON agent.ID = card.AGENT
-    INNER JOIN D_AGENT_PREGNANCY pregnancy
-      ON pregnancy.id = card.PREGNANCY
-    LEFT JOIN D_PREGC_OUT_REASONS reason
-      ON reason.ID = card.OUT_REASON
-  )
-  SELECT
-    SURNAME,
-    FIRSTNAME,
-    LASTNAME,
-    BIRTHDATE,
-    ENP,
-    SNILS,
-    DATE_IN,
-    DATE_OUT,
-    BEGIN_DATE,
-    END_DATE,
-    REASON
-  FROM PREGNANTS
-  WHERE 1 = 1
-    AND r = 1
-    AND ENP = :enp`;
+
+  const sqlFile = path.join(__dirname, '../sql', 'pregnants-info-by-enp.sql');
+  const sqlQuery = await fsPromises.readFile(sqlFile, 'utf8');
   let oracledb = require('oracledb');
-  let connection;
   try {
     oracledb
     .getConnection({
@@ -53,16 +18,24 @@ router.get('/', async function(req, res, next) {
       user: ORACLE_DB_USER,
       password: ORACLE_DB_PASS,
     })
-      .then((conn) => {
-        connection = conn;
+      .then((connection) => {
         connection
-        .execute(query, [enp])
+        .execute(sqlQuery, [enp])
         .then((result) => {
-          console.log('execute!');
-          // console.log(result);
-          const answer = result.rows[0];
-          console.log(answer);
-          res.send(answer);
+          if (!result.rows[0]) {
+            const notFound = {error: 'Not Found'};
+            res.json(notFound);
+          } else {
+            const found = result.rows[0];
+            const answer = {
+              CARD_DATE_START: found[0],
+              CARD_DATE_END: found[1],
+              PREGNANCY_DATE_START: found[2],
+              PREGNANCY_DATE_END: found[3],
+              REASON: found[4],
+            };
+            res.json(answer);
+          }
         })
         .catch((err) => {
           console.error('Can\'t execute!');
@@ -77,15 +50,6 @@ router.get('/', async function(req, res, next) {
     console.error(err);
     process.exit(1);
   }
-
-
-  // const { createdOn = getNowDate() } = req.query;
-  // const count = await req.db.collection('tasks')
-  //   .count({'createdOn': new RegExp(createdOn, 'gi')});
-  //   res.send({
-  //     date: createdOn,
-  //     tasks_count: count,
-  //   });
 });
 
 module.exports = router;
