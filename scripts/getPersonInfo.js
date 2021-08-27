1ae4cfc7819a2783a146286075ac771a7161563a
Alexander Shukan <avshukan@gmail.com>
2021-08-27 18:17:19 +1100

    add: inner table

--- scripts/getPersonInfo.js
+++ scripts/getPersonInfo.js
@@ -1,4 +1,6 @@
 'use strict';
+const VISIBLE_TD_COUNT = 5;
+
 async function getPersonInfo(snils, f, i, o, dr) {
   const url = `/get-person-info/api?snils=${snils}&f=${f}&i=${i}&o=${o}&dr=${dr}`;
   console.log('url', url);
@@ -21,24 +23,46 @@ async function getPersonInfo(snils, f, i, o, dr) {
     let rows = document.querySelector('tbody');
     data.forEach(item => {
       // добавляем полученные элементы в таблицу
-      rows.append(tr(item));
+      rows.append(tr_visible(item));
+      rows.append(tr_hidden(item));
     });
   }
 }
 
-function tr(item) {
-  const tr = document.createElement('tr');
-  Object.keys(item).forEach(key => {
-    tr.append(td(item[key]));
+function tr_visible(item) {
+  const tr_visible = document.createElement('tr');
+  Object.keys(item).filter((value, index) => index <= VISIBLE_TD_COUNT).forEach(key => {
+    tr_visible.append(get_td(item[key]));
   });
-  // tr.append(td(item[1]));
-  // tr.append(td(item[2]));
-  // tr.append(td(item[3]));
-  // tr.append(td(item[4]));
-  return tr;
+  tr_visible.append(get_td('Показать'));
+  return tr_visible;
 }
 
-function td(value) {
+function tr_hidden(item) {
+  const tbody = document.createElement('tbody');
+  Object.keys(item).filter((value, index) => index > VISIBLE_TD_COUNT).forEach((key, index) => {
+    const tr = document.createElement('tr');
+    tr.append(get_td(index));
+    tr.append(get_td(item[key]));
+    tbody.append(tr);
+  });
+  const table = document.createElement('table');
+  table.append(tbody);
+  const td = document.createElement('td');
+  td.setAttribute('colspan', `${VISIBLE_TD_COUNT + 2}`);
+  td.append(table);
+  const tr_hidden = document.createElement('tr');
+  tr_hidden.append(td);
+  return tr_hidden;
+}
+
+function table_hidden(data) {
+  const td = document.createElement('td');
+  td.append(value);
+  return td;
+}
+
+function get_td(value) {
   const td = document.createElement('td');
   td.append(value);
   return td;
